a94ccf5fac20527ccc4d2c8a51fd434b56f9fe5c
Rahul Banerjee <rbanerjee28@gmail.com>
2022-01-07 10:40:21 +0100

    updated running script

--- run_solvers.sh
+++ run_solvers.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
 printf "\nCleaning old files...\n"
-
 rm *.out
-
 printf "\nDone.\n"
 
 printf "\nCompiling program...\n"
-
 nvcc -std=c++17 -Xcompiler -fopenmp -I /usr/local/include/eigen3 main.cu -lcusolver -o cholesky.out
-
 printf "\nDone.\n"
 
 export OMP_NUM_THREADS=6
-
 ./cholesky.out
 
 printf "\nPlotting and saving graph.\n"
-
 python3 plot.py
-
 printf "\nDone.\n"
