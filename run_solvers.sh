4fb73266c7ab80e1bdcdea8831ad1e799721d7de
Rahul Banerjee <rbanerjee28@gmail.com>
2022-01-12 09:29:58 +0100

    corrected openmp flag

--- run_solvers.sh
+++ run_solvers.sh
@@ -5,7 +5,7 @@ rm *.out
 printf "\nDone.\n"
 
 printf "\nCompiling program...\n"
-nvcc -std=c++17 -O3 -Xcompiler -fopenmp,-march=native -I /usr/local/include/eigen3 main.cu -lcusolver -o cholesky.out
+nvcc -std=c++17 -O3 -Xcompiler=-fopenmp,-march=native -I /usr/local/include/eigen3 main.cu -lcusolver -o cholesky.out
 printf "\nDone.\n"
 
 export OMP_NUM_THREADS=6
